3719f06913fc181fca7df85ce714a9d52b701671
luis botelho <luisfellypebotelho@gmail.com>
2021-10-22 23:21:15 -0300

    romove slash to deploy

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { UnauthorizedInterceptor } from './interceptors/unauthorized.interceptor
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: ['http://localhost:3001', 'https://mod4frontend.herokuapp.com/'],
+      origin: ['http://localhost:3001', 'https://mod4frontend.herokuapp.com'],
     },
   });
 
